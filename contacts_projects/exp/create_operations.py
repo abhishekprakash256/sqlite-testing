ee349a585572c6bfd75f9930dae13cd7d3f407cf
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-06-01 23:47:15 -0400

    experimnets started

---
+++ contacts_projects/exp/create_operations.py
@@ -0,0 +1,82 @@
+"""
+make a python file to operation in the file 
+"""
+
+import sqlite3
+
+conn = sqlite3.connect("test.db")  #to make a connection to the db
+
+#make the cursor to tun the db commands 
+c = conn.cursor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+def create_customer(customer):
+    with connection:
+        cursor.execute("INSERT INTO customer VALUES (:first, :last, :age, :city, :country)", 
+        {'first':customer.first_name, 'last':customer.last_name,
+         'age':customer.age, 'city':customer.city, 'country':customer.country})
+    
+
+def get_customers(city):
+    cursor.execute("SELECT * FROM customer WHERE city=:city", {'city':city})
+    return cursor.fetchall()
+
+def update_city(customer, city):
+    with connection:
+        cursor.execute("""UPDATE customer SET city=:city 
+        WHERE first_name=:first AND last_name=:last""",
+        {'first':customer.first_name, 'last':customer.last_name, 'city':city})
+
+def delete_customer(customer):
+    with connection:
+        cursor.execute("DELETE FROM customer WHERE first_name=:first AND last_name=:last",
+        {'first':customer.first_name,'last':customer.last_name})
+
+customer_1 = Customer('john', 'doe', 30, 'perth', 'Australia')
+customer_2 = Customer('sara', 'migel', 25, 'perth', 'Australia')
+
+create_customer(customer_1)
+create_customer(customer_2)
+
+update_city(customer_1,'sydney')
+
+delete_customer(customer_2)
+
+print(get_customers('perth'))
+print(get_customers('sydney'))
+
+connection.close()
+'''
